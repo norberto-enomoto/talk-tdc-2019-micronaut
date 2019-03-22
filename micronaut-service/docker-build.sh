7e43ca9e0ae4aa46ed5dff07e9825b9ec15277a4
Luram Archanjo <luram.archanjo@sensdia.com>
2019-03-22 19:59:44 -0300

    Micronaut and GraalVM

--- micronaut-service/docker-build.sh
+++ micronaut-service/docker-build.sh
@@ -2,4 +2,4 @@ docker build . -t micronaut-service
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run --rm --net=host --name=micronaut-service micronaut-service"
+echo "    $ docker run -d --net=host --name=micronaut-service micronaut-service:latest"
