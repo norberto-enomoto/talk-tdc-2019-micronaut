3435c20c39bf59ce0a0a40547a72607b5e575890
Luram Archanjo <luram.archanjo@sensdia.com>
2019-03-23 18:15:18 -0300

    Native image setting heap size

--- micronaut-service/src/main/java/micronaut/service/Application.java
+++ micronaut-service/src/main/java/micronaut/service/Application.java
@@ -1,11 +1,14 @@
 package micronaut.service;
 
 import io.micronaut.runtime.Micronaut;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Application {
 
-    public static void main(String[] args) {
-        Micronaut.run(Application.class);
-    }
+  public static void main(String[] args) {
+    Micronaut.run(Application.class);
+    log.info("The application is using {} mb", (Runtime.getRuntime().totalMemory() / 1024 / 1024));
+  }
 
 }
